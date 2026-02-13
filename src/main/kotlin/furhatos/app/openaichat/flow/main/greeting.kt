92c0d7d7003816d75c8b9d8287cbd696220d6198
Edmondo <edmondo888@gmail.com>
2026-02-13 12:01:05 +0100

    First commit

---
+++ src/main/kotlin/furhatos/app/openaichat/flow/main/greeting.kt
@@ -0,0 +1,40 @@
+package furhatos.app.openaichat.flow
+
+import furhatos.app.openaichat.flow.chatbot.MainChat
+import furhatos.app.openaichat.setting.Persona
+
+import furhatos.app.openaichat.setting.hostPersona
+import furhatos.app.openaichat.setting.personas
+import furhatos.flow.kotlin.*
+import furhatos.records.Location
+
+val Greeting = state(Parent) {
+
+    onEntry {
+        furhat.attend(users.userClosestToPosition(Location(0.0, 0.0, 0.5)))
+        askForAnything("Ciao")
+        furhat.say("Sono Maurizio una persona che ama aiutare la gente, per farla stare meglio")
+        if (furhat.askYN("Sei pronto per partire?") == true) {
+
+                goto(ChoosePersona())
+
+
+        } else {
+            furhat.say("Okay, let's skip right ahead.")
+            goto(ChoosePersona())
+        }
+    }
+}
+
+var currentPersona: Persona = hostPersona
+
+fun ChoosePersona() = state(Parent) {
+
+    val persona = personas.firstOrNull { it.voice.first().isAvailable } ?: hostPersona
+
+    onEntry {
+        currentPersona = persona
+        //activate(persona) // Attiva la persona selezionata
+        goto(MainChat) // Vai alla conversazione principale
+    }
+}
